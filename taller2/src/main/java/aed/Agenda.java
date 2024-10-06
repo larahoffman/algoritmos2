c27928ecbad7e097e056e41a5dd45898069d2e94
larahoffman <larahoff02@gmail.com>
2024-10-06 17:35:52 -0300

    ejercicio 16 y 17

--- taller2/src/main/java/aed/Agenda.java
+++ taller2/src/main/java/aed/Agenda.java
@@ -1,21 +1,29 @@
 package aed;
 
 public class Agenda {
-    private Fecha fecha;
+    private Fecha fechaActual;
     private ArregloRedimensionableDeRecordatorios recordatorios;
     // Ejercicio 16
     public Agenda(Fecha fechaActual) {
-        this.fecha = new Fecha(fechaActual);
+        this.fechaActual = new Fecha(fechaActual);
+        this.recordatorios = new ArregloRedimensionableDeRecordatorios();
     }
-
+    // Ejercicio 17
     public void agregarRecordatorio(Recordatorio recordatorio) {
-        // Implementar
+        recordatorios.agregarAtras(recordatorio);
     }
 
     @Override
     public String toString() {
-        // Implementar
-        return "";
+        String arreglo = "";
+        arreglo += fechaActual.toString() + "\n";
+        arreglo += "=====\n";
+        for(int i = 0; i < recordatorios.longitud(); i++){
+            Recordatorio recordatorio = recordatorios.obtener(i);
+            arreglo += recordatorio.toString() + "\n";
+        };
+        
+        return arreglo;
     }
 
     public void incrementarDia() {
@@ -23,10 +31,27 @@ public class Agenda {
     }
 
     public Fecha fechaActual() {
-        Agenda agenda = new Agenda(fecha);
-        Fecha fechaAgenda = agenda.fecha;
+        Agenda agenda = new Agenda(fechaActual);
+        Fecha fechaAgenda = agenda.fechaActual;
         Fecha fechaActual = new Fecha(fechaAgenda);
         return fechaActual;
     }
+    public static void main(String[] args) {
+        // Crear una fecha inicial para la agenda
+        Fecha fechaInicial = new Fecha(10, 10);
 
+        // Crear la agenda con la fecha inicial
+        Agenda miAgenda = new Agenda(fechaInicial);
+
+        // Crear algunos recordatorios
+        Recordatorio recordatorio1 = new Recordatorio("Reunión con equipo", new Fecha(12, 10), new Horario(9, 30));
+        Recordatorio recordatorio2 = new Recordatorio("Cita médica", new Fecha(15, 10), new Horario(16, 45));
+        Recordatorio recordatorio3 = new Recordatorio("Entregar informe", new Fecha(20, 10), new Horario(11, 0));
+
+        miAgenda.agregarRecordatorio(recordatorio1);
+        miAgenda.agregarRecordatorio(recordatorio2);
+        miAgenda.agregarRecordatorio(recordatorio3);
+
+        System.out.println(miAgenda);
+    }
 }
