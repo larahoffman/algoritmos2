293671831cca326f659fdd455e8de4f305b02068
larahoffman <larahoff02@gmail.com>
2024-10-06 17:36:37 -0300

    quitado pruebas

--- taller2/src/main/java/aed/Agenda.java
+++ taller2/src/main/java/aed/Agenda.java
@@ -36,22 +36,5 @@ public class Agenda {
         Fecha fechaActual = new Fecha(fechaAgenda);
         return fechaActual;
     }
-    public static void main(String[] args) {
-        // Crear una fecha inicial para la agenda
-        Fecha fechaInicial = new Fecha(10, 10);
 
-        // Crear la agenda con la fecha inicial
-        Agenda miAgenda = new Agenda(fechaInicial);
-
-        // Crear algunos recordatorios
-        Recordatorio recordatorio1 = new Recordatorio("Reunión con equipo", new Fecha(12, 10), new Horario(9, 30));
-        Recordatorio recordatorio2 = new Recordatorio("Cita médica", new Fecha(15, 10), new Horario(16, 45));
-        Recordatorio recordatorio3 = new Recordatorio("Entregar informe", new Fecha(20, 10), new Horario(11, 0));
-
-        miAgenda.agregarRecordatorio(recordatorio1);
-        miAgenda.agregarRecordatorio(recordatorio2);
-        miAgenda.agregarRecordatorio(recordatorio3);
-
-        System.out.println(miAgenda);
-    }
 }
