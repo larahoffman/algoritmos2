6a8fea889afb479b01c3c8ec032efa0b2bf1bb82
larahoffman <larahoff02@gmail.com>
2024-10-18 21:05:30 -0300

    fixes

--- taller3/src/main/java/aed/ListaEnlazada.java
+++ taller3/src/main/java/aed/ListaEnlazada.java
@@ -39,55 +39,54 @@ public class ListaEnlazada<T> implements Secuencia<T> {
             primero.anterior = nuevo;
             primero = nuevo;
         }
+        tamanio++;
     }
-    // falta testear
+
     public void agregarAtras(T elem) {
-	Nodo<T> nuevo = new Nodo<T>(elem);
-	    if(primero == null){ // caso lista vacía
-		primero = nuevo;
-		ultimo = nuevo;
-	    }else{
-		ultimo.siguiente = nuevo;
-		nuevo.anterior = ultimo;
-		ultimo = nuevo;
-	    }
+        Nodo<T> nuevo = new Nodo<T>(elem);
+        if(primero == null){ // caso lista vacía
+            primero = nuevo;
+            ultimo = nuevo;
+        }else{
+            ultimo.siguiente = nuevo;
+            nuevo.anterior = ultimo;
+            ultimo = nuevo;
+        }
+        tamanio++;
 	}
-	// falta testear
+
 	public T obtener(int i) {
-	    Nodo<T> nodoActual;
-	    if(!((i > longitud()) || (i < 0))){ // acá valido que no esté fuera de rango
-                if (i < longitud() / 2) {
-                    nodoActual = primero;
-                    for (int j = 0; j < i; j++) {
-                        nodoActual = nodoActual.siguiente;
-                    }
-                } else {
-                    nodoActual = ultimo;
-                    for (int j = longitud - 1; j > i; j--) {
-                        nodoActual = nodoActual.anterior;
-                    }
-                }
-			}
-			return nodoActual.dato;
-		}
-		public Nodo<T> obtenerNodo(int i) {
-			Nodo<T> nodoActual;
-			if(!((i > longitud()) || (i < 0))){ // acá valido que no esté fuera de rango
-                if (i < longitud() / 2) {
-                    nodoActual = primero;
-                    for (int j = 0; j < i; j++) {
-                        nodoActual = nodoActual.siguiente;
-                    }
-                } else {
-                    nodoActual = ultimo;
-                    for (int j = longitud - 1; j > i; j--) {
-                        nodoActual = nodoActual.anterior;
-                    }
-                }
-			}
-			return nodoActual;
-		}
-        // falta testear
+	    Nodo<T> nodoActual = null;
+        if (i < longitud() / 2) {
+            nodoActual = primero;
+            for (int j = 0; j < i; j++) {
+                nodoActual = nodoActual.siguiente;
+            }
+        } else {
+            nodoActual = ultimo;
+            for (int j = longitud() - 1; j > i; j--) {
+                nodoActual = nodoActual.anterior;
+            }
+        }
+		return nodoActual.dato;
+	}
+
+	public Nodo<T> obtenerNodo(int i) {
+		Nodo<T> nodoActual = null;
+        if (i < longitud() / 2) {
+            nodoActual = primero;
+            for (int j = 0; j < i; j++) {
+                nodoActual = nodoActual.siguiente;
+            }
+        } else {
+            nodoActual = ultimo;
+            for (int j = longitud() - 1; j > i; j--) {
+                nodoActual = nodoActual.anterior;
+            }
+        }
+        return nodoActual;
+	}
+
 	public void eliminar(int i) {
             Nodo<T> nodoActual = obtenerNodo(i);
 
@@ -150,5 +149,22 @@ public class ListaEnlazada<T> implements Secuencia<T> {
     public Iterador<T> iterador() {
 	    throw new UnsupportedOperationException("No implementada aun");
     }
+    public static void main(String[] args) {
+        ListaEnlazada<Integer> lista = new ListaEnlazada<>();
 
+        lista.agregarAdelante(42);
+        lista.agregarAtras(3);
+
+
+        // lista.agregarAlFinal(10);
+        // lista.agregarAlFinal(20);
+        // lista.agregarAlInicio(5);
+
+        System.out.println(lista.longitud()); // Imprime: 5 10 20
+        System.out.println(lista.obtener(1)); // Imprime: 20 10 5
+
+        // lista.eliminar(10);
+        // lista.mostrar(); // Imprime: 5 20
+    }
 }
+
