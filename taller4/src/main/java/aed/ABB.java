a0d5b07967983ca23a0fa474e770644bbb53e4fc
larahoffman <larahoff02@gmail.com>
2024-10-26 21:12:34 -0300

    atributos y constructor

--- taller4/src/main/java/aed/ABB.java
+++ taller4/src/main/java/aed/ABB.java
@@ -6,15 +6,28 @@ import java.util.*;
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     // Agregar atributos privados del Conjunto
+    private Nodo raiz;
+    private int cardinal;
+    private int altura;
 
     private class Nodo {
-        // Agregar atributos privados del Nodo
+        T dato;
+        Nodo izquierdo;
+        Nodo derecho;
+        Nodo padre;
 
-        // Crear Constructor del nodo
+        public Nodo(T d){
+            this.dato = d;
+            this.izquierdo = null;
+            this.derecho = null;
+            this.padre = null;
+        }
     }
 
     public ABB() {
-        throw new UnsupportedOperationException("No implementada aun");
+        this.raiz = null;
+        this.cardinal = 0;
+        this.altura = 0;
     }
 
     public int cardinal() {
